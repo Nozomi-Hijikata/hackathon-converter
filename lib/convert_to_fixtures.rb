4e30870de2bf7e19e6eb0958ed09f1e354679b47
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2023-07-20 14:05:54 +0900

    🐛ymlへ変換時の小数点とnilのときに対応

--- lib/convert_to_fixtures.rb
+++ lib/convert_to_fixtures.rb
@@ -9,8 +9,11 @@ def convert_to_fixtures(file_path)
 
   CSV.foreach(file_path, headers: true) do |row|
     fixtures["#{fixtures_section_name}_#{row[0]}"] = row.to_hash.map do |k,v|
-      if v.match(/\A\d*\Z/)
-        value  = v.to_i
+      next [k,v] if v.nil?
+      if v.match(/\A\d*\.\d*\Z/) #小数点を考慮
+        value  = v.to_f
+      elsif v.match(/\A\d+\Z/) #整数の場合
+        value = v.to_i
       else
         value = v
       end
